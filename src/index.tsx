da2033d47dcabd80bb47ce2fdd5e08fb46942d2f
Zeye Gu <zeyegu@gmail.com>
2024-09-04 21:59:29 -0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+
+const App = () => {
+  const ref = useRef<any>();
+
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  // build: S => (g(), $.build(S))
+  // serve : ƒ serve(S, k)
+  // stop : ƒ stop()
+  // transform : ƒ transform(S, k)
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el!);
+
+root.render(<App />);
