de354adc310595b0d5bdd7ce23c005501be7c143
Zeye Gu <zeyegu@gmail.com>
2024-09-25 04:04:57 -0700

    extract logic from index.tsx

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,8 @@ import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import { useRef } from "react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import "./code-editor.css";
+import "./syntax.css";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -26,19 +28,27 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     const unfomatted = editorRef.current.getModel().getValue();
 
     // format that value
-    const formatted = prettier.format(unfomatted, {
-      parser: "babel",
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      // doubleQuotes: true,
-    });
+    const formatted = prettier
+      .format(unfomatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        // doubleQuotes: true,
+      })
+      //prettier automatically add a new line. It's a work aroun to remove it.
+      .replace(/\n$/, "");
     // set the formatted value back to the editor
     editorRef.current.setValue(formatted);
   };
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={editorDidMount}
         value={initialValue}
--- src/index.tsx
+++ src/index.tsx
@@ -1,100 +1,33 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
-import * as esbuild from "esbuild-wasm";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
+import bundle from "./bundler";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
-
+  const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  // build: S => (g(), $.build(S))
-  // serve : ƒ serve(S, k)
-  // stop : ƒ stop()
-  // transform : ƒ transform(S, k)
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        gobal: "window",
-      },
-    });
-
-    // execute js code
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    const output = await bundle(input);
+    setCode(output);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener(
-            "message",
-            (event) => {
-              try {
-                eval(event.data);
-              } catch (error) {
-                const root = document.querySelector("#root");
-                root.innerHTML =
-                  "<div style='color: red;'><h4> Runtime Error </h4>" +
-                  error +
-                  "</div>";
-                console.error(error);
-              }
-            },
-            false
-          );
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
-      <CodeEditor initialValue="" onChange={(value) => setInput(value)} />
-      <textarea
-        onChange={(e) => setInput(e.target.value)}
-        value={input}
-      ></textarea>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
