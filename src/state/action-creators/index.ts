92f474bc2ad97ab9ba184e96ea1ac636d45b5050
Zeye Gu <zeyegu@gmail.com>
2024-12-21 06:46:30 -0800

    part of last commit

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -3,7 +3,7 @@ import {
   Action,
   UpdateCellAction,
   DeleteCellAction,
-  InsertCellBeforeAction,
+  InsertCellAfterAction,
   MoveCellAction,
 } from "../actions";
 import { Direction } from "../actions";
@@ -35,12 +35,12 @@ export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   };
 };
 
-export const InsertCellBefore = (
-  id: string,
+export const insertCellAfter = (
+  id: string | null,
   cellType: CellTypes
-): InsertCellBeforeAction => {
+): InsertCellAfterAction => {
   return {
-    type: ActionType.INSERT_CELL_BEFORE,
+    type: ActionType.INSERT_CELL_AFTER,
     payload: {
       id,
       type: cellType,
