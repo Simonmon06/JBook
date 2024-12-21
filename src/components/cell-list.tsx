b2707baf4a7dceeb424b583a4cd583c7507804dd
Zeye Gu <zeyegu@gmail.com>
2024-12-21 06:44:36 -0800

    add code/text cell buttons

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,16 +1,26 @@
 import CellListItem from "./cell-list-item";
-
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import AddCell from "./add-cell";
+import { Fragment } from "react/jsx-runtime";
+
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
     return order.map((id) => data[id]);
   });
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <Fragment key={cell.id}>
+      <CellListItem cell={cell} />
+      <AddCell previousCellId={cell.id} />
+    </Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderedCells}
+    </div>
+  );
 };
 
 export default CellList;
